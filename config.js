c9ad1174ded3e49362460455d8e96f007aada887
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-16 21:19:05 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DPRO_DISPOSAL_CONFIG = Object.freeze({
+  API_BASE: "https://dpro-disposal-line-api.dpromstk2000.workers.dev",
+  COMPANY_CODE: "dpro_disposal_demo",
+  VERSION: "DISPOSAL-3-CUSTOMER-20260716",
+  DEFAULT_LINE_USER_ID: ""
+});
